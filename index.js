3dd0f5209b575c1fa9dc733e3b517c8b43e9419a
Abena Adu <abeanadu@gmail.com>
2022-10-11 04:20:33 -0400

    commit tree b41c632d96bf32456a548f0c031da9d0b8623c38

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let someValue;
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return "This one is on me!" ;
+  } 
+  else if (someValue >= 400 && someValue <= 2000){
+    return "That will be twenty bucks."
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (someValue >= 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+const cityName = "NYC";
+function ternaryCheckCity(cityName){
+  return (cityName === "NYC") ? "Ok, sounds good." : "No go." ;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const tipAmount = "generous";
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount) {
+    case "generous" :
+      return ("Thank you so much.");
+    case "not as generous" :
+      return ("Thank you.");
+    default :
+      return ("Bye.")
+  } 
 }
